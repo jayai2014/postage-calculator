236c37dd7f67cc9c80d78b1fded4547836513957
jayai2014 <dafu.ai819@gmail.com>
2017-09-05 14:04:30 +1000

    Add one unit test

--- app/src/test/java/au/edu/unimelb/comp30022/controllertesting/ControllerTest.java
+++ app/src/test/java/au/edu/unimelb/comp30022/controllertesting/ControllerTest.java
@@ -1,22 +1,65 @@
 package au.edu.unimelb.comp30022.controllertesting;
 
+import android.location.Location;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
-
-/**
- * Created by william on 31/8/17.
- */
+@RunWith(MockitoJUnitRunner.class)
 public class ControllerTest {
+
+    //@InjectMocks annotation is used to create and inject the mock object
+    @InjectMocks
+    Controller controller;
+
+    //@Mock annotation is used to create the mock object to be injected
+    @Mock
+    PostageRateCalculator postageRateCalculator;
+
+    @Mock
+    PostcodeValidator postCodeValidator;
+
+    @Mock
+    AddressTools addressTools;
+
+    @Mock
+    Location location;
+
+    @Test
+    public void testCostLabel() {
+        Mockito.when(postageRateCalculator.computeCost(
+                Mockito.any(Location.class),
+                Mockito.any(Location.class)))
+                .thenReturn(5);
+        Mockito.when(postCodeValidator.isValid(Mockito.any(String.class)))
+                .thenReturn(true);
+        Mockito.when(addressTools.locationFromAddress(Mockito.any(Address.class)))
+                .thenReturn(location);
+
+        controller = new Controller(addressTools, postCodeValidator, postageRateCalculator);
+
+        controller.calculateButtonPressed();
+
+        //test the cost label
+        Assert.assertEquals(controller.costLabel.getText(), "$5");
+    }
+
     @Before
     public void setUp() throws Exception {
-
+        UI.addWidget("SOURCE_POST_CODE", new EditText());
+        UI.addWidget("DESTINATION_POST_CODE", new EditText());
+        UI.addWidget("COST_LABEL", new TextView());
     }
 
     @After
     public void tearDown() throws Exception {
-
+        // don't need to do anything
     }
-
 }
